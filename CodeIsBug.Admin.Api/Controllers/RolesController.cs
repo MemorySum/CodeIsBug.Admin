cf768a418a8054a177ea0fa066d83793d6d7ba10
Admin <1433028369@qq.com>
2020-10-04 20:03:45 +0800

    添加角色管理

---
+++ CodeIsBug.Admin.Api/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeIsBug.Admin.Api.Controllers
+{
+    /// <summary>
+    /// 角色管理
+    /// </summary>
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+    }
+}
---
+++ CodeIsBug.Admin.Services/Service/RolesService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using CodeIsBug.Admin.Models.DbContext;
+using CodeIsBug.Admin.Models.Models;
+
+namespace CodeIsBug.Admin.Services.Service
+{
+    /// <summary>
+    /// 角色Service
+    /// </summary>
+    public class RolesService:DataContext<ESysRoles>
+    {
+        
+    }
+}
