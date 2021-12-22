a510e2c7be8a1ad0feed4cd77c41086343bc1d82
admin <1433028369@qq.com>
2021-12-22 22:12:17 +0800

    net5

--- CodeIsBug.Admin.Common/Helper/VerifyCodeHelper.cs
+++ CodeIsBug.Admin.Common/Helper/VerifyCodeHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.DrawingCore;
-using System.DrawingCore.Drawing2D;
-using System.DrawingCore.Imaging;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 
 namespace CodeIsBug.Admin.Common.Helper
@@ -290,4 +290,4 @@ namespace CodeIsBug.Admin.Common.Helper
 
         #endregion
     }
-}                                          
+}
